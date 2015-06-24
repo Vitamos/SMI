60a86fafdbb5e84b922c4ffe6a33b9b08dd3eb5b
Gomes <a38432@alunos.isel.pt>
2015-06-24 03:30:11 +0100

    v0.02

---
+++ menus/users.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <?php
+        ?>
+    <body>
+        <button>Adicionar Utilizador</button>
+        <button>Pesquisar Utilizador</button>
+        <section id="result">asdf</section>
+    </body>
+</html>
+
